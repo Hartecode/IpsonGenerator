3b9277d292dbc4abc23f7e904b487fbb68ee676f
Sean Harte <scsharte@gmail.com>
2019-08-13 22:13:02 -0400

    - moved the questions to a array - added a back grounstyle

--- index.js
+++ index.js
@@ -19,9 +19,23 @@ app.set('view engine', 'pug');
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
+    const questionAnswer = [
+        {
+            question: 'What is Panda Ipsum?',
+            answer:'A placeholder text for Panda lovers. Great for any project where you need visual mockups and/or to previewing layouts.'
+        },
+        {
+            question: 'Why use Panda Ipsum?',
+            answer: 'Becasue Pandas are awesome. Enough said!'
+        },
+        {
+            question: 'How it works?',
+            answer: "Just select the amount of paragraphs you want and click generate. Sorry we couldn't make it much easier the that."
+        }
+    ];
     const oneParagraphIpsum = ipsumInstance.generateText(true, ipsumInstance.paragraphLength());
     console.log(oneParagraphIpsum)
-    res.render('index', {ipsum: oneParagraphIpsum});
+    res.render('index', {questionAnswer, ipsum: oneParagraphIpsum});
 });
 
 app.get('/api', (req, res) => {
